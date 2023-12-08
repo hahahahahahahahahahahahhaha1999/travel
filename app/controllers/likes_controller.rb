b85f6dbb602cd2fc52fbcf9d0e9afe3cde704a5c
hahahahahahahahahahahahhaha1999 <148528205+hahahahahahahahahahahahhaha1999@users.noreply.github.com>
2023-12-08 11:17:37 +0900

    いいねカウント数まで表示

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,23 @@
 class LikesController < ApplicationController
+  before_action :set_tweet
+
   def create
     like = current_user.likes.build(article_id: params[:article_id])
     like.save
-    redirect_to 
+    respond_to do |format|
+      format.js
+    end 
   end
 
   def destroy
     like = Like.find_by(article_id: params[:article_id], user_id: current_user.id)
     like.destroy
-    redirect_to 
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def set_tweet
+    @article = Article.find(params[:article_id])
   end
 end
